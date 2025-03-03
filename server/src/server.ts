123522b6d8c499f3c21878d265bd0b5ffbb960b8
Kato Hirohito <Kato.Hirohito@exc.epson.co.jp>
2025-03-03 16:09:45 +0900

    check current dirctory

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,12 +3,14 @@ import { compress } from "hono/compress";
 import { load } from "mod";
 import { registerRoutes } from "./routes/index.ts";
 import { serveStatic } from "https://deno.land/x/hono/middleware.ts";
+import process from "node:process";
 
 // Honoアプリを生成
 const app = new Hono({ strict: true });
 app.use(compress());
 
 // ルーティングを登録
+console.log("current dir: " + process.cwd());
 registerRoutes(app);
 app.get("/static/*", serveStatic({ root: "./" }));
 
