d281ef476eda1a8a8a595fda8f2d03b990dd6763
Kato Hirohito <Kato.Hirohito@exc.epson.co.jp>
2025-04-13 16:51:10 +0900

    remove React logo displayed on the frontend UI

--- frontend/cherry_manager/src/App.tsx
+++ frontend/cherry_manager/src/App.tsx
@@ -1,4 +1,3 @@
-import reactLogo from './assets/react.svg';
 import './App.css';
 import './components/ExcelDropzone';
 import ExcelDropzone from './components/ExcelDropzone';
@@ -9,11 +8,6 @@ function App() {
     return (
         <>
             <ExcelDataProvider>
-                <div>
-                    <a href="https://react.dev" target="_blank">
-                        <img src={reactLogo} className="logo react" alt="React logo" />
-                    </a>
-                </div>
                 <ExcelDropzone />
                 <TableView />
                 <p className="read-the-docs">2025/03/12 プロトタイプ</p>
